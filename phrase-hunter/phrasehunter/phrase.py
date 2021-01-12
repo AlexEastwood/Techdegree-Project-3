7c3f584aaebdaf17212cd2593adcc358b3024f85
AlexEastwood <76944389+AlexEastwood@users.noreply.github.com>
2021-01-12 06:10:52 +0000

    Updated repo root

---
+++ phrase-hunter/phrasehunter/phrase.py
@@ -0,0 +1,17 @@
+# Create your Phrase class logic here.
+class Phrase(self):
+    def __init__(self, phrase):
+        phrase = self.phrase
+        
+    def display():
+        #print phrase to console, using underscores as unguessed letters
+        pass
+
+    def check_letter(letter):
+        #check if letter selected by user matches a letter in the phrase
+        #also check if the letter has already been guessed
+        pass
+        
+    def check_complete():
+        #check if the whole phrase has been guessed
+        pass
